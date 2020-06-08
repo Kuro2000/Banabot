a1388eef808a5b71b7aa1f0ffcfbb3acb6734ce3
Than Duc Thang <ducthang200023@gmail.com>
2020-06-08 18:47:48 +0700

    Custom prefix support + Some code optimizations

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,21 @@
-desc='Hiển thị danh sách lệnh';
+// Hiển thị danh sách lệnh
 
-const fs = require('fs');
+const fs = require('fs')
+const config = require('../config.json')
 
-module.exports = (client,message) => {
-    const channel = client.channels.cache.get("706914192803758181") //bot-commands channel
-
+module.exports = (message) => {
+    const channel = message.channel //Get channel
     fs.readdir("./commands",(err,files)=>{
         files.forEach(file=>{
             const eventName = file.split(".")[0]; //Get name of each commands through fs.readdir function
             eventName[0].toUpperCase();
 
             fs.readFile(`./commands/${file}`,'utf-8',(err,data)=>{
-                const eventDesc = ((data.split('\n')[0]).match(/'([^']+)'/)[1]); //Get the description as the first line of commands file
-                channel.send(`! ${eventName}: ${eventDesc}`);
+                //const eventDesc = ((data.split('\n')[0]).match(/'([^']+)'/)[1]); //Get the description as the first line of commands file
+                const eventDesc = (data.split('\n')[0]).slice(3);
+                channel.send(`> ${config.prefix}${eventName}: ${eventDesc}`);
             });
             
         })
     }) 
-    
 }
--- events/message.js
+++ events/message.js
@@ -3,26 +3,37 @@ const role = require("../commands/role")
 const inv = require("../commands/inv")
 const vote = require("../commands/vote")
 const help = require("../commands/help")
+const config = require("../config.json")
 
+const prefix = config.prefix
 module.exports = (client, message) => {
-  if (message.content.startsWith("!kick")){ //Kick function
+  if (!message.content.startsWith(prefix) || message.author.bot) return; //Return none if prefix not detected or if author is also a bot
+
+  if (message.content.startsWith(prefix + "ping") && message.author.id == config.ownerID){ //Test function for server owner
+    return message.channel.send({embed:{
+      color: 3447003,
+      description: "A very simple Embed!"
+    }})
+  }
+
+  if (message.content.startsWith(prefix + "kick")){ //Kick function.
     return kick(message)
   }
   
-  if (message.content.startsWith("!role")){ //Set role function
-    return role(client,message)
+  if (message.content.startsWith(prefix + "role")){ //Set role function, only useable in my server.
+    return role(message)
   }
 
-  if (message.content.startsWith("!inv")){ //Get invite link function
+  if (message.content.startsWith(prefix + "inv")){ //Get invite link function.
     return inv(message)
   }
 
-  if(message.content.startsWith("!vote")){ //Vote function
+  if(message.content.startsWith(prefix + "vote")){ //Vote function.
     return vote(message)
   }
 
-  if(message.content.startsWith("!help")){ //Get list of function
-    return help(client,message)
+  if(message.content.startsWith(prefix + "help")){ //Get list of function.
+    return help(message)
   }
 
 }
