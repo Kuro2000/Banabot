649d6fbbc45343e3638053893c5eb5d4a7470aa3
Than Duc Thang <ducthang200023@gmail.com>
2020-06-08 18:49:41 +0700

    Added custom message when user leave

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,5 @@
+module.exports = (client, member) => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+    if(!channel) return;
+    channel.send(`${member} đã rời khỏi server`)
+}
