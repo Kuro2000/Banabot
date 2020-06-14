077ea51299a7afc25920e2f1e3d570a77535eabd
Than Duc Thang <ducthang200023@gmail.com>
2020-06-14 14:58:06 +0700

    Updated features

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Discord-Bot
-Created a Discord Bot for auto moderation
-Currently deploying on Heroku, with BOT_TOKEN provided in configvars
+Created a Discord Bot for auto moderation\n
+Currently deploying on Heroku, with `BOT_TOKEN` and `MONGO_URI` provided in configvars
 
 Using ```npm run dev``` to start using live server
 
-Currently features:
+Features:
 - help: Display commands list
 - invite: Create an invite link
 - kick: Kick an user
 - vote: Create quick YES/NO vote poll for a specified topic
+- report: Send reports to server
+- stats: Check info of user
+- balance: Commands regard to server's economy system
+    +) Balance altering for admins
+    +) Economy leaderboards
+- Some **admin-only** commands
--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,25 @@
-// Hiển thị danh sách lệnh
-
-const fs = require('fs')
+const Discord = require('discord.js')
 const config = require('../config.json')
+const prefix = config.prefix
 
 module.exports = (message) => {
-    const channel = message.channel //Get channel
-    fs.readdir("./commands",(err,files)=>{
-        files.forEach(file=>{
-            const eventName = file.split(".")[0]; //Get name of each commands through fs.readdir function
-            eventName[0].toUpperCase();
+    let helpEmbed = new Discord.MessageEmbed()
+    .setColor(3447003)
+    .setTitle(`Commands list`)
+    .setDescription(`**📜Các câu hiện có trong server**`)
+    .setThumbnail(`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png`)
+    .addFields(
+        {name: `🙋‍♂️ **${prefix}help**`, value: "Danh sách các lệnh"},
+        {name: `⚰️ **${prefix}role <role>**`, value: "Set role cho thành viên, hiện có: `btcm`,`nccm` và `guest`"},
+        {name: `📊 **${prefix}stats**`, value: "Khởi tạo và hiển thị các thông tin người dùng"},
+        {name: `💰 **${prefix}balance <câu lệnh>**`, value: "Các lệnh về server economy, câu lệnh hiện có: `ranking` và `daily`"},
+        {name: `🗳️ **${prefix}vote <chủ đề>**`, value: "Tạo nhanh bộ vote YES/NO"},
+        {name: `💌 **${prefix}inv**`, value: "Tạo mã mời vào server", inline: true},
+        {name: `🦶 **${prefix}kick @user**`, value: "Kick một người khỏi server", inline: true},
+        {name: `📝 **${prefix}report <nội dung>**`, value: "Gửi báo cáo, góp ý server"},
+        {name: "😎 **Các lệnh admin khác**", value: `Hiện có lệnh *${prefix}send*, *${prefix}balance god*`},
+    )
+    .setFooter("Discord BOT by Kuro")
 
-            fs.readFile(`./commands/${file}`,'utf-8',(err,data)=>{
-                //const eventDesc = ((data.split('\n')[0]).match(/'([^']+)'/)[1]); //Get the description as the first line of commands file
-                const eventDesc = (data.split('\n')[0]).slice(3);
-                channel.send(`> ${config.prefix}${eventName}: ${eventDesc}`);
-            });
-            
-        })
-    }) 
+    message.channel.send(helpEmbed)
 }
