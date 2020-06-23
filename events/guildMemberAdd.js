3588989ecd24970196367ed52b6b58abe6c2453f
Than Duc Thang <ducthang200023@gmail.com>
2020-06-24 00:07:59 +0700

    Init/Delete user data on join/leave

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+const User = require('../models/userModels')
 
 module.exports = (client, member) => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
@@ -7,5 +9,23 @@ module.exports = (client, member) => {
         channel.send(`Chào mừng bạn đến server, ${member}, một chú BOT 🤖 đã PM cho bạn cách thức để gia nhập server, hãy làm theo hướng dẫn`);
         member.send("Hãy chọn Role của bạn bằng cách **Nhập lệnh sau** vào kênh text *bot-commands*:\nNhập ```!role btcm```nếu bạn thuộc tiểu ban **Biên tập Chuyên môn** ⚰️⚰️\n```!role nccm```nếu bạn thuộc tiểu ban **Nghiên cứu và Ứng dụng Chuyên môn** ⚰️⚰️\n```!role guest```nếu bạn là Khách 🐵🐵\nBạn nên check Pinned messages của box chat *bot-commands* để biết thêm về một số lệnh khác")    
     }
+
+    //Save info to DB
+    const user = new User({
+        _id: mongoose.Types.ObjectId(),
+        userID: member.id,
+        username: member.username,
+        role: "default",
+        balance: 0,
+        level: 1,
+        exp: 0,
+        lastAttendance: 0
+    })
+    
+    user.save()
+    .then(result => {
+        console.log("Sucessfully init DB data, user: "+result.userID)
+    })
+    .catch(err => console.log(err))
    
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose')
+const User = require('../models/userModels')
+
 module.exports = (client, member) => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome')
     if(!channel) return;
     channel.send(`${member} đã rời khỏi server`)
+    User.remove({userID: member.id}, err =>{
+        if(!err){return console.log(`Successfully deleted DB data, user: ${member.id}`)}
+        console.log("Error: " +err)
+    })
 }
