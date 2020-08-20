6e1a95d0cd4ae7683aa094a96c04a7fe3079e62e
Than Duc Thang <ducthang200023@gmail.com>
2020-08-21 00:21:24 +0700

    🎨 Optimize project structure, added Canvas

--- bot.js
+++ bot.js
@@ -1,24 +1,41 @@
-require("dotenv").config()
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+require('dotenv').config();
+
+const fs = require('fs');
+const requireDir = require('require-dir');
+const dir = requireDir('./commands', { recurse: true, extensions: ['.js'] });
+
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-const logger = require('./winston')
 
-//Establish Discord Client connection
-fs.readdir("./events",(err,files)=>{
-    files.forEach(file=>{
-        const eventHandler = require(`./events/${file}`);
-        const eventName = file.split(".")[0]
-        client.on(eventName,(...args)=> eventHandler(client,...args))
-    })
-})
+const logger = require('./winston');
 
-client.login(process.env.BOT_TOKEN)
+// Establish Discord Client connection
 
-//Establish Database connection
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => logger.info('Database: Connected!'));
+// Events handlers
+fs.readdir('./events', (err, files)=>{
+	files.forEach(file=>{
+		const eventHandler = require(`./events/${file}`);
+		const eventName = file.split('.')[0];
+		client.on(eventName, (...args)=> eventHandler(client, ...args));
+	});
+});
+
+// Commands handlers
+Object.keys(dir).forEach(category =>{
+	Object.keys(dir[category]).forEach(commandName =>{
+		const command = require(`./commands/${category}/${commandName}.js`);
+		client.commands.set(commandName, command);
+	});
+});
+
+client.login(process.env.BOT_TOKEN);
+
+// Establish Database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => logger.info('Database: Connected!'));
 db.on('error', err=>{
-    logger.error(`Database: connection error: ${err.message}`)
-})
+	logger.error(`Database: connection error: ${err.message}`);
+});
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose')
-const User = require('../models/userModels')
-const config = require('../config.json')
-const logger = require('../winston')
+const User = require('../models/userModels');
+const config = require('../config.json');
+const logger = require('../winston');
 
 module.exports = (client, member) => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === config.welcomeChannel);
-    if(!channel) return logger.warn("No welcome channel found");
-    channel.send(`${member} đã rời khỏi server`)
-    User.deleteOne({userID: member.id}, err =>{
-        if(!err){return logger.info(`Database: Successfully deleted DB data, user: ${member.id}`)}
-        logger.error("Error: " +err)
-    })
-}
+	const channel = member.guild.channels.cache.find(ch => ch.name === config.welcomeChannel);
+	if(!channel) return logger.warn('No welcome channel found');
+	channel.send(`${member} đã rời khỏi server`);
+	User.deleteOne({ userID: member.id }, err =>{
+		if(!err) {return logger.info(`Database: Successfully deleted DB data, user: ${member.id}`);}
+		logger.error('Error: ' + err);
+	});
+};
