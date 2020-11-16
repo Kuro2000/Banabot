e7fbf0f2e5a5ff71934edfd52aca96c3c70e193f
Than Duc Thang <ducthang200023@gmail.com>
2020-11-16 17:55:56 +0700

    🔃 A little bit refactoring

--- events/message.js
+++ events/message.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const config = require('../config.json');
 const logger = require('../winston');
 
@@ -8,6 +9,9 @@ module.exports = (client, message) => {
 	// Return none if prefix not detected or if author is also a bot
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+	// Also return if commands not issued in #bot-commands channel, ignore admins
+	if (!message.member.hasPermission('ADMINISTRATOR') && message.channel.name != config.botChannels[1].name) return;
+
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
@@ -16,21 +20,53 @@ module.exports = (client, message) => {
 	// Return none if the command is not found
 	if(!command) return;
 
+	// Check the availability of Database-related commands (Specifically Economy commands)
+	if(!process.env.MONGO_URI && command.category === 'economy') {
+		logger.warn('Database: No URI provided');
+		return message.channel.send('Chưa có Database');
+	}
+
+	// Check for owner ID if commands in the Superuser category is issued
+	if(command.category === 'superuser' && message.author.id != config.ownerID) {
+		return message.channel.send('Bạn không có quyền thực hiện lệnh trên');
+	}
+
+	// Author's Supportted server only
+	if (command.category === 'nec' && message.guild.id != config.NECGuildID) return message.channel.send('Server không hỗ trợ lệnh này');
+
+	// Moderator permissions check
+	if (command.category === 'moderation' && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Bạn không có quyền thực hiện lệnh trên');
+
+	// Not available yet
+	if (command.category === 'economy') return message.channel.send('Chưa dùng được đâu 😢');
+
 	// Check if the command require arguments
 	if (command.argRequired && !args.length) {
-		let reply = 'Nhập lệnh ``!help`` để xem lại cú pháp';
+		// Show the usage if specified
+		const argEmbed = new Discord.MessageEmbed()
+			.setColor(config.embedColors.info)
+			.setTitle('Sai cú pháp')
+			.setFooter('Banabot by Kuro');
+
 		if(command.usage) {
-			reply += `\nCách sử dụng đúng: \`${prefix}${command.name} ${command.usage}\``;
+			const reply = `${prefix}${command.name} ${command.usage}`;
+			argEmbed.setDescription('Cách sử dụng đúng: ' + '``' + reply + '``');
 		}
 
-		return message.channel.send(reply);
+		return message.channel.send(argEmbed);
 	}
+
 	// Check if the command work in DMs
 	if (command.guildOnly && message.channel.type === 'dm') {
 		return message.reply('Lệnh không khả dụng trong DMs');
 	}
 
+	// Try executing the command when all conditions are fullfiled
 	try {
+		if(command.name == 'test') {
+			return command.execute(message, client, args);
+		}
+
 		command.execute(message, args);
 	}
 	catch (error) {
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
 const logger = require('../winston');
+const config = require('../config.json');
 
 module.exports = client => {
-	client.user.setActivity('Điều hòa thông minh');
+	client.user.setActivity(`[${config.prefix}] I'm a BOT!`);
 	logger.info(`Discord: Ready to serve as ${client.user.tag}!`);
 	logger.info(`Discord: Available on ${client.guilds.cache.size} servers, for ${client.users.cache.size} users!`);
 };
